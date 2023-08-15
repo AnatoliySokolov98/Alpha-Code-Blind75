6d1a5c95cf8f6ff43a56e8915ecb9c6fbcc8af2c
Anatoliy <anatoliysokolov98@gmail.com>
2023-08-15 13:24:25 -0700

    finished Container with most water

---
+++ js-files/3.container-with-most-water.js
@@ -0,0 +1,13 @@
+"use strict";
+function maxArea(height) {
+    let left = 0, right = height.length - 1, res = 0;
+    while (left < right) {
+        res = Math.max(res, Math.min(height[left], height[right]) * (right - left));
+        if (height[left] < height[right])
+            left++;
+        else
+            right--;
+    }
+    return res;
+}
+;
