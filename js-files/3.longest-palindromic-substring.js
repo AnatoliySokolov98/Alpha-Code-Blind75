0ffe06d5a486d1652403dcffaab2f47ea852a24b
Anatoliy <anatoliysokolov98@gmail.com>
2023-08-14 21:57:19 -0700

    finished longest palindromic substring

---
+++ js-files/3.longest-palindromic-substring.js
@@ -0,0 +1,21 @@
+"use strict";
+function longestPalindrome(s) {
+    let resLeft = 0, resRight = 0;
+    function buildPalindrome(left, right) {
+        while (left >= 0 && right < s.length && s[left] == s[right]) {
+            if (resRight - resLeft < right - left) {
+                resRight = right;
+                resLeft = left;
+            }
+            left--;
+            right++;
+        }
+    }
+    for (let i = 0; i < s.length; i++) {
+        buildPalindrome(i, i);
+        buildPalindrome(i, i - 1);
+    }
+    return s.slice(resLeft, resRight + 1);
+}
+;
+console.log(longestPalindrome('amanaplanacanalpanama'));
