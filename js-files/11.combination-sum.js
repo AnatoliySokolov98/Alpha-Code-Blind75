d946017609a36911b18b711145ff6aa11b4b4543
Anatoliy <anatoliysokolov98@gmail.com>
2023-08-23 13:04:59 -0700

    finished combination sum

---
+++ js-files/11.combination-sum.js
@@ -0,0 +1,30 @@
+"use strict";
+function combinationSum(candidates, target) {
+    const cache = {};
+    const cacheKey = (index, target) => `${index}-${target}`;
+    candidates.sort((a, b) => a - b);
+    const dp = (index, target) => {
+        const key = cacheKey(index, target);
+        if (key in cache) {
+            return cache[key];
+        }
+        if (index === candidates.length || candidates[index] > target) {
+            return [];
+        }
+        if (candidates[index] === target) {
+            return [[target]];
+        }
+        let res = [];
+        let dpRes = dp(index, target - candidates[index]);
+        for (let item of dpRes) {
+            res.push(item.concat(candidates[index]));
+        }
+        dpRes = dp(index + 1, target);
+        for (let item of dpRes) {
+            res.push(item.slice());
+        }
+        cache[key] = res;
+        return res;
+    };
+    return dp(0, target);
+}
--- js-files/leetcode-problem.js
+++ js-files/leetcode-problem.js
@@ -1,17 +1,1 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mergeKLists = void 0;
-const _8_merge_two_sorted_lists_1 = require("./8.merge-two-sorted-lists");
-function mergeKLists(lists) {
-    let interval = 1;
-    while (interval < lists.length) {
-        for (let i = 0; i < lists.length - interval; i += interval * 2) {
-            lists[i] = (0, _8_merge_two_sorted_lists_1.mergeTwoLists)(lists[i], lists[i + interval]);
-            lists[i + interval] = null;
-        }
-        interval *= 2;
-    }
-    return lists.length > 0 ? lists[0] : null;
-}
-exports.mergeKLists = mergeKLists;
-;
