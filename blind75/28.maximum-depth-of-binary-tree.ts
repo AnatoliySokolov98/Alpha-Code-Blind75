ee6dc785f5243aff22d46c8fbeba38e49791590d
Anatoliy <anatoliysokolov98@gmail.com>
2023-10-03 11:54:56 -0700

    finished invert binary tree

--- blind75/28.maximum-depth-of-binary-tree.ts
+++ blind75/28.maximum-depth-of-binary-tree.ts
@@ -3,3 +3,4 @@ import { TreeNode } from "./data-structures/tree-node";
 export function maxDepth(root: TreeNode | null): number {
   return root ? 1 + Math.max(maxDepth(root.left), maxDepth(root.right)) : 0;
 };
+
