10129a41ae42863baf2f6c94ff69f8abf7ebc596
Anatoliy <anatoliysokolov98@gmail.com>
2023-10-05 12:04:06 -0700

    finished lca of bst

---
+++ blind75/53.lca-of-bst.ts
@@ -0,0 +1,13 @@
+import { TreeNode } from "./data-structures/tree-node";
+
+export function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+
+	while (p && q && root) {
+        if(p.val < root.val && q.val < root.val)
+            root = root.left;
+        else if (p.val > root.val && q.val > root.val)
+            root = root.right;
+        else
+            return root;
+    }
+};
